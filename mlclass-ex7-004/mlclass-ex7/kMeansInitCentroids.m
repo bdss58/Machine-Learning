da8dd4413235f8994031aa3653c80b4c3025c31f
bdss58 <809808709@qq.com>
2014-09-16 22:02:39 +0800

    modify previous files and add ex7

---
+++ mlclass-ex7-004/mlclass-ex7/kMeansInitCentroids.m
@@ -0,0 +1,29 @@
+function centroids = kMeansInitCentroids(X, K)
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%used in K-Means on the dataset X
+%   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
+%   used with the K-Means on the dataset X
+%
+
+% You should return this values correctly
+centroids = zeros(K, size(X, 2));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should set centroids to randomly chosen examples from
+%               the dataset X
+%
+
+
+for j=1:K,
+    points_set = double(idx == j);
+    centroids(j,:) = sum(X .* [points_set points_set]) ./ sum(points_set);
+end
+
+
+
+
+
+% =============================================================
+
+end
+
